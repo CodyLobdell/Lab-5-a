2454125c799985ca0abed4b3caa2b523f48d3df2
Cody Lobdell <thecreator85@gmail.com>
2023-01-27 17:29:58 -0800

    added answers to main

--- js/app.js
+++ js/app.js
@@ -29,15 +29,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  function multiply(a, b) { //eslint-disable-line
-    let multiplyValue = a * b;
-    let message = `The sum of ${a} and ${b} is ${multiplyValue}.`;
-    return [multiplyValue, message];
-  
+  let multiplyValue = a * b;
+  let message = `The sum of ${a} and ${b} is ${multiplyValue}.`;
+  return [multiplyValue, message];
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
